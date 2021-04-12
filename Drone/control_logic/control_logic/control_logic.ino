62a1f1e92078ac4511620f8526113bf5eae7ae83
sikora-toma <ts50570@fer.hr>
2021-04-12 11:36:11 +0200

    Control logic development setup

---
+++ Drone/control_logic/control_logic/control_logic.ino
@@ -0,0 +1,14 @@
+/*
+ * This code serves as a mockup for developing the control logic
+ * for a drone based on Arduino. When ready, the code will be transfered to the control_code. 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

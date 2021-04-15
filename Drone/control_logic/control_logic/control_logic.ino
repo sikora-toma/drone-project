ef324e455053454b263913131eec36b84506712a
sikora-toma <ts50570@fer.hr>
2021-04-15 10:56:02 +0200

    Update control_logic.ino

--- Drone/control_logic/control_logic/control_logic.ino
+++ Drone/control_logic/control_logic/control_logic.ino
@@ -4,7 +4,7 @@
  */
 
 float pitch_goal, roll_goal, yaw_goal;
-float pitch_curr, roll_curr, yaw_curr;
+float pitch_input, roll_input, yaw_input;
 
 // setup the gains for all PID values for each control variable
 float pid_pitch_p_gain, pid_pitch_i_gain, pid_pitch_d_gain;
@@ -16,7 +16,7 @@ float pid_pitch_output, pid_roll_output, pid_yaw_output;
 float pid_pitch_max, pid_roll_max, pid_yaw_max;
 // setup integral and derivative auxiliary variables
 float pid_pitch_i_mem, pid_roll_i_mem, pid_yaw_i_mem, pid_pitch_d_last, pid__pitch_last_error, pid__roll_last_error, pid__yaw_last_error;
-float pid_pitch_error, pid_roll_error, pid__yaw_last_error;
+float pid_pitch_error, pid_roll_error, pid_yaw_error;
 // setup the motor output variables
 int motor1_output, motor2_output, motor3_output, motor4_output;
 
@@ -28,9 +28,53 @@ void loop() {
 	// calculate the difference between the desired and current values
 	// calculate the PID values for each motor
 
+    // P calculation
+  pid_pitch_error = pitch_input - pitch_goal;
+  pid_roll_error = roll_input - roll_goal;
+  pid_yaw_error = yaw_input - yaw_goal;
+  
+    // I calculation
+  pid_pitch_i_mem += pid_i_gain_pitch * pid_error_temp;
+  if(pid_pitch_i_mem > pid_max_pitch)pid_i_mem_pitch = pid_max_pitch;
+  else if(pid_pitch_i_mem < pid_max_pitch * -1)pid_pitch_i_mem = pid_max_pitch * -1;
+
+  pid_roll_i_mem += pid_i_gain_pitch * pid_error_temp;
+  if(pid_roll_i_mem > pid_max_pitch)pid_roll_i_mem = pid_max_pitch;
+  else if(pid_i_mem_pitch < pid_max_pitch * -1)pid_roll_i_mem = pid_max_pitch * -1;
+  
+  pid_yaw_i_mem += pid_yaw_i_mem * pid_error_temp;
+  if(pid_yaw_i_mem > pid_max_pitch)pid_yaw_i_mem = pid_max_pitch;
+  else if(pid_yaw_i_mem < pid_max_pitch * -1)pid_yaw_i_mem = pid_max_pitch * -1;
+
+  // full PID calculation
+  pid_pitch_output =  pid_pitch_gain * pid_pitch_error + pid_pitch_gain * pid_pitch_i_mem + pid_pitch_d_gain * (pid_pitch_error-pid_pitch_last_error);
+  pid_roll_output =   pid_roll_gain * pid_roll_error   + pid_roll_gain * pid_roll_i_mem   + pid_roll_d_gain * (pid_roll_error-pid_roll_last_error);
+  pid_yaw_output =    pid_yaw_gain * pid_yaw_error     + pid_yaw_gain * pid_yaw_i_mem     + pid_yaw_d_gain * (pid_yaw_error-pid_yaw_last_error);
+
+
 	// calculate the output for each motor
 	motor1_output = 0;
 	motor2_output = 0;
 	motor3_output = 0;
 	motor4_output = 0;
+
+  
+  
+  if(<THROTTLE_MIN) motor1_output = THROTTLE_MIN;
+  if(>THROTTLE_MAX) motor1_output = THROTTLE_MAX;
+  
+  if(<THROTTLE_MIN) motor2_output = THROTTLE_MIN;
+  if(>THROTTLE_MAX) motor2_output = THROTTLE_MAX;
+  
+  if(<THROTTLE_MIN) motor3_output = THROTTLE_MIN;
+  if(>THROTTLE_MAX) motor3_output = THROTTLE_MAX;
+  
+  if(<THROTTLE_MIN) motor4_output = THROTTLE_MIN;
+  if(>THROTTLE_MAX) motor4_output = THROTTLE_MAX;
+
+  // D step  
+  pid_pitch_last_error = pid_pitch_error;
+  pid_roll_last_error = pid_roll_error;
+  pid__yaw_last_error = pid__yaw_error;
+
 }

04226d1ea465a1d8df22ce721b3fee19e57ebb40
sikora-toma <ts50570@fer.hr>
2021-04-18 22:13:58 +0200

    Update control_logic.ino

--- Drone/control_logic/control_logic/control_logic.ino
+++ Drone/control_logic/control_logic/control_logic.ino
@@ -79,4 +79,38 @@ void loop() {
   pid_roll_last_error = pid_roll_error;
   pid__yaw_last_error = pid__yaw_error;
 
+
+  
+  if (start == 2){                                                          //The motors are started.
+    if (throttle > 1800) throttle = 1800;                                   //We need some room to keep full control at full throttle.
+    esc_1 = throttle - pid_output_pitch + pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 1 (front-right - CCW)
+    esc_2 = throttle + pid_output_pitch + pid_output_roll + pid_output_yaw; //Calculate the pulse for esc 2 (rear-right - CW)
+    esc_3 = throttle + pid_output_pitch - pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 3 (rear-left - CCW)
+    esc_4 = throttle - pid_output_pitch - pid_output_roll + pid_output_yaw; //Calculate the pulse for esc 4 (front-left - CW)
+
+    if (battery_voltage < 1240 && battery_voltage > 800){                   //Is the battery connected?
+      esc_1 += esc_1 * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-1 pulse for voltage drop.
+      esc_2 += esc_2 * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-2 pulse for voltage drop.
+      esc_3 += esc_3 * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-3 pulse for voltage drop.
+      esc_4 += esc_4 * ((1240 - battery_voltage)/(float)3500);              //Compensate the esc-4 pulse for voltage drop.
+    } 
+
+    if (esc_1 < 1100) esc_1 = 1100;                                         //Keep the motors running.
+    if (esc_2 < 1100) esc_2 = 1100;                                         //Keep the motors running.
+    if (esc_3 < 1100) esc_3 = 1100;                                         //Keep the motors running.
+    if (esc_4 < 1100) esc_4 = 1100;                                         //Keep the motors running.
+
+    if(esc_1 > 2000)esc_1 = 2000;                                           //Limit the esc-1 pulse to 2000us.
+    if(esc_2 > 2000)esc_2 = 2000;                                           //Limit the esc-2 pulse to 2000us.
+    if(esc_3 > 2000)esc_3 = 2000;                                           //Limit the esc-3 pulse to 2000us.
+    if(esc_4 > 2000)esc_4 = 2000;                                           //Limit the esc-4 pulse to 2000us.  
+  }
+
+  else{
+    esc_1 = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-1.
+    esc_2 = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-2.
+    esc_3 = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-3.
+    esc_4 = 1000;                                                           //If start is not 2 keep a 1000us pulse for ess-4.
+  }
+
 }
